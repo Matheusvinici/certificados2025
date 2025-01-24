a2a399c104ada155067a7244f40e4fb912a126ad
Matheus Vinicius Vidal de Andrade <matheus2vandrade@gmail.com>
2025-01-24 11:27:35 -0300

    alteracoes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,15 +2,17 @@
 <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
-        <!-- Painel Inicial -->
+        <!-- Painel Inicial - Acesso para formador, user comum e admin -->
+        @if(Auth::user()->role == 'admin' || Auth::user()->role == 'user' || Auth::user()->role == 'formador')
         <li class="nav-item">
             <a href="{{ route('home') }}" class="nav-link text-white">
                 <i class="nav-icon fas fa-tachometer-alt text-white"></i>
                 <p>{{ __('Painel Inicial') }}</p>
             </a>
         </li>
+        @endif
 
-        <!-- Opção: Servidores -->
+        <!-- Opção: Servidores - Apenas para admin -->
         @if(Auth::user()->role == 'admin')
         <li class="nav-item">
             <a href="{{ route('users.index') }}" class="nav-link text-white">
@@ -20,7 +22,7 @@
         </li>
         @endif
 
-        <!-- Opção: Cursos -->
+        <!-- Opção: Cursos - Apenas para admin -->
         @if(Auth::user()->role == 'admin')
         <li class="nav-item">
             <a href="{{ route('cursos.index') }}" class="nav-link text-white">
@@ -30,27 +32,33 @@
         </li>
         @endif
 
-        <!-- Opção: Inscrições -->
+        <!-- Opção: Inscrições - Apenas para admin e usuário comum -->
+        @if(Auth::user()->role == 'admin' || Auth::user()->role == 'user')
         <li class="nav-item">
             <a href="{{ route('inscricoes.index') }}" class="nav-link text-white">
                 <i class="nav-icon fas fa-edit text-white"></i>
-                <p>
-                    {{ Auth::user()->role == 'admin' ? __('Inscrições') : __('Minhas Inscrições') }}
-                </p>
+                <p>{{ __('Inscrições') }}</p>
             </a>
         </li>
+        @endif
 
-        <!-- Opção: Certificados -->
+        <!-- Opção: Certificados - Apenas para admin e usuário comum (com título alterado) -->
+        @if(Auth::user()->role == 'admin' || Auth::user()->role == 'user')
         <li class="nav-item">
             <a href="{{ route('certificados.index') }}" class="nav-link text-white">
                 <i class="nav-icon fas fa-file-alt text-white"></i>
                 <p>
-                    {{ Auth::user()->role == 'admin' ? __('Certificados') : __('Meus Certificados') }}
+                    @if(Auth::user()->role == 'admin')
+                        {{ __('Certificados') }}
+                    @else
+                        {{ __('Meus Certificados') }}
+                    @endif
                 </p>
             </a>
         </li>
+        @endif
 
-        <!-- Opção: Formadores -->
+        <!-- Opção: Formadores - Apenas para admin e formadores -->
         @if(Auth::user()->role == 'admin' || Auth::user()->role == 'formador')
         <li class="nav-item">
             <a href="{{ route('formador.index') }}" class="nav-link text-white">
@@ -60,18 +68,17 @@
         </li>
         @endif
 
-                    <!-- Opção: Relatórios -->
-            @if(Auth::user()->role == 'admin')
-            <li class="nav-item">
-                <a href="{{ route('relatorios.frequencia') }}" class="nav-link text-white">
-                    <i class="nav-icon fas fa-chart-bar text-white"></i>
-                    <p>{{ __('Relatórios de Frequência') }}</p>
-                </a>
-            </li>
-            @endif
+        <!-- Opção: Relatórios de Frequência - Apenas para admin e formadores -->
+        @if(Auth::user()->role == 'admin' || Auth::user()->role == 'formador')
+        <li class="nav-item">
+            <a href="{{ route('relatorios.frequencia') }}" class="nav-link text-white">
+                <i class="nav-icon fas fa-chart-bar text-white"></i>
+                <p>{{ __('Relatórios de Frequência') }}</p>
+            </a>
+        </li>
+        @endif
 
-
-        <!-- About Us -->
+        <!-- About Us - Acesso para todos -->
         <li class="nav-item">
             <a href="{{ route('about') }}" class="nav-link text-white">
                 <i class="nav-icon far fa-address-card text-white"></i>
